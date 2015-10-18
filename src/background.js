4868a3e9c4160738ba0a275183f3ea54c82bce51
Uchechukwu Nnadi <ucheunnadi@gmail.com>
2015-10-17 17:40:40 -0700

    [update] update to extention - double request bug

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ var bodyWidth;    // For storing the width of wiki's displaced body, so
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
   
   if(tab.url.match(/wikipedia.org\/wiki/g) && tab.status === 'loading'){
-
+    console.log('again')
     chrome.tabs.executeScript(null, {file: "./assets/jquery.min.js"});
     chrome.tabs.executeScript(null, {file: "./assets/d3.min.js"});
     chrome.tabs.executeScript(null, {file: "./assets/react.js"});
@@ -23,6 +23,7 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
     chrome.pageAction.show(tab.id);
 
     if (localStorage['immedia.chrome'] === 'on') {                       // If they previously had the extension activated, when 
+      console.log('running extention.js')
       chrome.tabs.executeScript(null, {file: "./src/extension.js"});     // going to a new page the extension will be activated
     }
   }
@@ -31,10 +32,10 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
 
 chrome.runtime.onMessage.addListener(function(message, sender){
   var searchTerm = parseUrl(sender.url).replace(/\s\(.*$/,'').toLowerCase();
-  if (searchTerm === 'Main Page') {
+  if (searchTerm === 'main page') {
     searchTerm = 'immediahomepage';
   }
-  handleQuery(message, searchTerm);
+  handleQuery(message, searchTerm, sender);
   bodyWidth = message.bodyWidth;
 });
 
@@ -62,18 +63,11 @@ function parseUrl(url){
   return resultURL.replace(/_/g, ' ');
 }
 
-function handleQuery(searchQuery, searchTerm){
+function handleQuery(searchQuery, searchTerm, sender){
   searchQuery.searchTerm = searchTerm;  
   $.post(searchQuery.url, searchQuery)
     .done(function(response) {
-      checkTabs(response);
+      chrome.tabs.sendMessage(sender.tab.id, response);
       console.log('Post request was a Success...Here is the response', response)
    });
-}
-
-function checkTabs(response){
-  chrome.tabs.query({active:true}, function(tabs){
-    //chrome.tabs.executeScript(null, {file: "./assets/twitter.js"});
-    chrome.tabs.sendMessage(tabs[0].id, response);               
-  });
 }
